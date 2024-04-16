365126cbb605d887eff949bd967cf771dc945989
triangle1009 <chenwilly1009@gmail.com>
2024-04-16 20:40:02 +0800

    first commit

---
+++ .vitepress/theme/components/Comments.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="gitalk-container"></div>
+</template>
+<script lang="ts" setup>
+import "gitalk/dist/gitalk.css";
+import Gitalk from "gitalk";
+import { onContentUpdated, useRouter } from "vitepress";
+
+// const { route, go } = useRouter();
+function deleteChild(element: HTMLDivElement | null) {
+  let child = element?.lastElementChild;
+  while (child) {
+    element?.removeChild(child);
+    child = element?.lastElementChild;
+  }
+}
+onContentUpdated(() => {
+  // reset gittalk element for update
+  const element = document.querySelector("#gitalk-container");
+  if (!element) {
+    return;
+  }
+  deleteChild(element);
+  const gitalk = new Gitalk({
+    clientID: "",
+    clientSecret: "",
+    repo: "blog-comments",
+    owner: "triangle",
+    admin: ["triangle"],
+    id: location.pathname.substring(0, 50), // Ensure uniqueness and length less than 50
+    language: "zh-CN",
+    distractionFreeMode: true, // Facebook-like distraction free mode
+  });
+  gitalk.render("gitalk-container");
+});
+</script>
+<style scoped></style>
---
+++ .vitepress/theme/components/ShareCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="shareCard">
+    <h1 class="title">
+      <span>Hi, I'm triangle1009 🗻	</span>
+    </h1>
+    <p class="description">
+      我也不知道弄這個要幹甚麼.
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup></script>
+<style scoped>
+.shareCard {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 0 15px;
+  text-align: center;
+}
+.title {
+  display: flex;
+  align-items: center;
+  font-size: 2.2em;
+  font-weight: bold;
+}
+.description {
+  margin: 1em 0;
+}
+</style>
